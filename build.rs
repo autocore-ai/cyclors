a87edb1a0a94cc7d263391f7ff2268bb763ce31c
luxiang.zhou <luxiang.zhou@autocore.ai>
2021-03-11 13:10:06 +0800

    🐞fix(build): build in ros2 foxy

--- build.rs
+++ build.rs
@@ -4,6 +4,10 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let ros_lib_x86_64_linux_gnu = if let Ok(path) = env::var("ROS_DISTRO") {
+    	format!("/opt/ros/{}/lib/x86_64-linux-gnu", path)
+    } else { "/opt/ros/foxy/lib/x86_64-linux-gnu".into() };
+    println!("cargo:rustc-link-search={}", ros_lib_x86_64_linux_gnu);
     println!("cargo:rustc-link-lib=ddsc");
     println!("cargo:rustc-link-lib=cdds-util");
     let incl_dir = if let Ok(path) = env::var("CYCLONE_INCLUDE") {
@@ -14,6 +18,7 @@ fn main() {
     } else { "-L/usr/local/lib".into() };
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .clang_arg("-I/usr/lib/gcc/x86_64-linux-gnu/9/include")
 	.clang_arg(incl_dir)
 	.clang_arg(lib_dir)
         .generate_comments(false)
